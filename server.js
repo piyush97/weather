287dc57d7c88c1409ada894186d40530ea4406fd
Piyush Mehta <me@piyushmehta.com>
2018-03-25 15:41:14 +0530

    Port number added

--- server.js
+++ server.js
@@ -35,5 +35,5 @@ app.post('/', function (req, res) {
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Weather App is running on port 3000')
 })
